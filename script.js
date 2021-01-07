57c1fdf650a1a401ef2e8e7a3468a9d29f520b29
Mateusz Choma <kontakt@coderoad.pl>
2021-01-07 19:21:47 +0100

    fetching number from db

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
+const DB_URL = 'https://coderoad--sandbox-default-rtdb.firebaseio.com/counter-1/.json'
+
 const numberElement = document.querySelector('.counter__number')
 const incButtonElement = document.querySelector('.counter__button--inc')
 
-numberElement.innerHTML = '5'
+const updateNumber = (number) => {
+    numberElement.innerHTML = number
+}
+
+const fetchNumber = () => {
+    return fetch(DB_URL)
+        .then((r) => r.json())
+        .then((number) => updateNumber(number))
+}
+
+fetchNumber()
